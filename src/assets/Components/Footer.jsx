b67afb88ab39a7f8716bcba41abe84e01b9909ec
Ignacio Espósito <sgtech-dev@serviguidetech.com>
2024-01-01 23:11:43 +0100

    V3.14 responsive

--- src/assets/Components/Footer.jsx
+++ src/assets/Components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="flex h-48 bg-neutral-800 items-center justify-end">
-      <span className="font-semibold uppercase text-white">
-          Hecho con ♥ para SysArmyy sin fines comerciales
+    <div className="flex h-48 bg-neutral-800 items-center sm:justify-center md:justify-end p-4">
+      <span className="font-semibold uppercase sm:text-md md:text-xl text-white">
+          Hecho con ♥ para SysArmy sin fines comerciales
         </span>
     </div>
   )
